4094dbc2de265506ba74d5ef09144657b48e7c11
Ed Eykholt <ed.eykholt@gmail.com>
2024-06-14 12:06:41 -0700

    Added PAGE_EVENT_TYPE, PAGE_POST_TYPE

--- KeriAuth.BrowserExtension/wwwroot/scripts/ContentScript.ts
+++ KeriAuth.BrowserExtension/wwwroot/scripts/ContentScript.ts
@@ -20,6 +20,62 @@ interface EventData {
     [key: string]: any;
 }
 
+// TODO move to common file(s)
+
+// Note this is called TAB_STATE and others in the signify-browser-extension
+enum PAGE_EVENT_TYPE {
+    SELECT_IDENTIFIER = "select-identifier",
+    SELECT_CREDENTIAL = "select-credential",
+    SELECT_ID_CRED = "select-aid-or-credential",
+    SELECT_AUTO_SIGNIN = "select-auto-signin",
+    NONE = "none",
+    VENDOR_INFO = "vendor-info",
+    FETCH_RESOURCE = "fetch-resource",
+    AUTO_SIGNIN_SIG = "auto-signin-sig",
+}
+
+enum PAGE_POST_TYPE {
+    SIGNIFY_EXT = "signify-extension",
+    SELECT_AUTO_SIGNIN = "select-auto-signin",
+    SIGNIFY_SIGNATURE1 = "signify-signature",
+    SIGNIFY_SIGNATURE2 = "signify-signature",
+}
+
+// interfaces for the messages posted to the web page
+interface BaseMessage {
+    version: string;
+    type: PAGE_POST_TYPE;
+}
+
+interface SignifyExtensionMessage extends BaseMessage {
+    type: typeof PAGE_POST_TYPE.SIGNIFY_EXT;
+    data: {
+        extensionId: string;
+    };
+}
+
+interface SignifySignature1Message extends BaseMessage {
+    type: typeof PAGE_POST_TYPE.SIGNIFY_SIGNATURE1;
+    data: any;
+    requestId: string;
+    rurl: string;
+}
+
+interface SignifySignature2Message extends BaseMessage {
+    type: typeof PAGE_POST_TYPE.SIGNIFY_SIGNATURE2;
+    requestId: string;
+    data: any;
+}
+
+interface SignifyAutoSigninMessage extends BaseMessage {
+    type: typeof PAGE_POST_TYPE.SELECT_AUTO_SIGNIN;
+    requestId: string;
+    rurl: string;
+}
+
+// Union type for all possible messages that can be sent to the web page
+type PageMessage = SignifyExtensionMessage | SignifySignature1Message | SignifySignature2Message | SignifyAutoSigninMessage;
+
 // Handle messages from web page
 window.addEventListener(
     "message",
@@ -28,9 +84,22 @@ window.addEventListener(
         if (event.source !== window) {
             return;
         }
-        console.log("KERI Auth content script received message from page: " + event.data.type);
-        console.log(event.data);
-        return;
+        console.log("KERI Auth cs received from page: " + event.data.type);
+        console.log(event.data); // as object
+
+        switch (event.data.type) {
+            case PAGE_EVENT_TYPE.SELECT_IDENTIFIER:
+            case PAGE_EVENT_TYPE.SELECT_CREDENTIAL:
+            case PAGE_EVENT_TYPE.SELECT_ID_CRED:
+            case PAGE_EVENT_TYPE.SELECT_AUTO_SIGNIN:
+            case PAGE_EVENT_TYPE.NONE:
+            case PAGE_EVENT_TYPE.VENDOR_INFO:
+            case PAGE_EVENT_TYPE.FETCH_RESOURCE:
+            case PAGE_EVENT_TYPE.AUTO_SIGNIN_SIG:
+            default:
+                // TODO forward to extension
+                return;
+        }
     }
 );
 
@@ -42,16 +111,17 @@ chrome.runtime.onMessage.addListener(async function (
 ) {
     if (sender.id === chrome.runtime.id) {
         console.log(
-            "KERI Auth content script received message from extension: " +
+            "KERI Auth cs received from extension: " +
             message.type +
             ":" +
             message.subtype
         );
+        console.log(message); // as object
     }
 });
 
-function advertize(): void {
-    console.log("KERI Auth content script advertized its extensionId: " + chrome.runtime.id);
+function advertise(): void {
+    console.log("KERI Auth content script advertised its extensionId: " + chrome.runtime.id);
     window.postMessage(
         {
             type: "signify-extension",
@@ -63,6 +133,7 @@ function advertize(): void {
     );
 }
 
+
 // Delay in order for polaris-web module to be loaded and ready to receive the message.
 // TODO find a more deterministic approach vs delay?
-setTimeout(advertize, 1000);
+setTimeout(advertise, 1000);
