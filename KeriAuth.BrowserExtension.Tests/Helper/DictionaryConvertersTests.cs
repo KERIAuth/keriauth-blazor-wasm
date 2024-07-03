ca1dd866b5bfcca84de7259fea2f7dca88cd48a4
Ed Eykholt <ed.eykholt@gmail.com>
2024-07-02 18:57:08 -0700

    Added dictionaryConverter to be used for ACDC Credential

---
+++ KeriAuth.BrowserExtension.Tests/Helper/DictionaryConvertersTests.cs
@@ -0,0 +1,97 @@
+﻿using KeriAuth.BrowserExtension.Helper.DictionaryConverters;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace KeriAuth.BrowserExtension.Tests.Helper
+{
+    public class DictionaryConvertersTests
+    {
+        private readonly JsonSerializerOptions _options;
+
+        public DictionaryConvertersTests()
+        {
+            _options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                Converters = { new DictionaryConverter() }
+            };
+        }
+
+        [Fact]
+        public void GetValueByPathTest()
+        {
+            string jsonString = @"{
+            ""id"": 1,
+            ""name"": ""Foo1"",
+            ""nested"": {
+                ""value"": ""NestedValue1"",
+                ""deepNested"": {
+                    ""anotherValue"": ""DeepNestedValue1"",
+                    ""collection"": [
+                        {
+                            ""itemId"": 101,
+                            ""itemName"": ""Item1""
+                        },
+                        {
+                            ""itemId"": 102,
+                            ""itemName"": ""Item2""
+                        }
+                    ]
+                }
+            }}";
+
+            Dictionary<string, object> foo = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString, _options);
+
+            // Test id
+            var idTypedValue = DictionaryConverter.GetValueByPath(foo, "id");
+            Assert.NotNull(idTypedValue);
+            Assert.Equal(typeof(long), idTypedValue.Type);
+            Assert.Equal(1L, idTypedValue.Value);
+
+            // Test name
+            var nameTypedValue = DictionaryConverter.GetValueByPath(foo, "name");
+            Assert.NotNull(nameTypedValue);
+            Assert.Equal(typeof(string), nameTypedValue.Type);
+            Assert.Equal("Foo1", nameTypedValue.Value);
+
+            // Test nested value
+            var nestedValueTypedValue = DictionaryConverter.GetValueByPath(foo, "nested.value");
+            Assert.NotNull(nestedValueTypedValue);
+            Assert.Equal(typeof(string), nestedValueTypedValue.Type);
+            Assert.Equal("NestedValue1", nestedValueTypedValue.Value);
+
+            // Test deep nested value
+            var deepNestedValueTypedValue = DictionaryConverter.GetValueByPath(foo, "nested.deepNested.anotherValue");
+            Assert.NotNull(deepNestedValueTypedValue);
+            Assert.Equal(typeof(string), deepNestedValueTypedValue.Type);
+            Assert.Equal("DeepNestedValue1", deepNestedValueTypedValue.Value);
+
+            // Test collection
+            var collectionTypedValue = DictionaryConverter.GetValueByPath(foo, "nested.deepNested.collection");
+            Assert.NotNull(collectionTypedValue);
+            Assert.Equal(typeof(List<object>), collectionTypedValue.Type);
+            var collection = collectionTypedValue.Value as List<object>;
+            Assert.NotNull(collection);
+            Assert.Equal(2, collection.Count);
+
+            // Test collection item 1
+            Assert.IsType<Dictionary<string, object>>(collection[0]);
+            var firstItem = collection[0] as Dictionary<string, object>;
+            Assert.NotNull(firstItem);
+            Assert.Equal(101L, firstItem["itemId"]);
+            Assert.Equal("Item1", firstItem["itemName"]);
+
+            // Test collection item 2
+            Assert.IsType<Dictionary<string, object>>(collection[1]);
+            var secondItem = collection[1] as Dictionary<string, object>;
+            Assert.NotNull(secondItem);
+            Assert.Equal(102L, secondItem["itemId"]);
+            Assert.Equal("Item2", secondItem["itemName"]);
+        }
+    }
+}
---
+++ KeriAuth.BrowserExtension/Helper/DictionaryConverter.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace KeriAuth.BrowserExtension.Helper.DictionaryConverters
+{
+    public class TypedValue
+    {
+        public object Value { get; set; }
+        public Type Type { get; set; }
+
+        public TypedValue(object value, Type type)
+        {
+            Value = value;
+            Type = type;
+        }
+    }
+
+    public class DictionaryConverter : JsonConverter<Dictionary<string, object>>
+    {
+        public override Dictionary<string, object> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            var dictionary = new Dictionary<string, object>();
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    return dictionary;
+                }
+
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                string propertyName = reader.GetString();
+                if (propertyName == null)
+                {
+                    throw new JsonException("Property name is null");
+                }
+
+                reader.Read();
+
+                object value = ReadValue(ref reader, options);
+                dictionary.Add(propertyName, value);
+            }
+
+            return dictionary;
+        }
+
+        private object ReadValue(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    return reader.GetString();
+                case JsonTokenType.Number:
+                    if (reader.TryGetInt64(out long l))
+                    {
+                        return l;
+                    }
+                    return reader.GetDouble();
+                case JsonTokenType.StartObject:
+                    return JsonSerializer.Deserialize<Dictionary<string, object>>(ref reader, options);
+                case JsonTokenType.StartArray:
+                    var list = new List<object>();
+                    while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
+                    {
+                        list.Add(ReadValue(ref reader, options));
+                    }
+                    return list;
+                case JsonTokenType.Null:
+                    return null;
+                default:
+                    throw new JsonException($"Unexpected token parsing JSON: {reader.TokenType}");
+            }
+        }
+
+        public static TypedValue GetValueByPath(Dictionary<string, object> dictionary, string path)
+        {
+            string[] keys = path.Split('.');
+            object current = dictionary;
+
+            foreach (var key in keys)
+            {
+                if (current is Dictionary<string, object> currentDict && currentDict.TryGetValue(key, out var value))
+                {
+                    current = value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return new TypedValue(current, current?.GetType());
+        }
+
+        public override void Write(Utf8JsonWriter writer, Dictionary<string, object> value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
