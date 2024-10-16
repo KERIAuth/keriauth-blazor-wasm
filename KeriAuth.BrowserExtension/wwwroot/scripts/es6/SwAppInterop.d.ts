098c9011c532d923dc99260538ac2fe1a7a0b3fe
Ed Eykholt <ed.eykholt@gmail.com>
2024-10-16 11:51:49 -0700

    gitignore

--- KeriAuth.BrowserExtension/wwwroot/scripts/es6/SwAppInterop.d.ts
+++
@@ -1,9 +0,0 @@
-interface DotNetObjectReference<T = any> {
-    invokeMethodAsync: (methodName: string, ...args: any[]) => Promise<void>;
-}
-export declare const SwAppInteropModule: {
-    initializeMessaging: (dotNetObjectReference: DotNetObjectReference, tabId: String) => chrome.runtime.Port | null;
-    sendMessageToServiceWorker: (port: chrome.runtime.Port, jsonReplyMessageData: string) => void;
-    parseJson: <T>(jsonString: string) => T | null;
-};
-export default SwAppInteropModule;
