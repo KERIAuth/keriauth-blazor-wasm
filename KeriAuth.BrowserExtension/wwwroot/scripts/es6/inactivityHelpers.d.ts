941ce200a1b01ac9d0852251d2bb82662979c535
edeykholt <ed.eykholt@gmail.com>
2024-12-07 14:18:40 -0800

    gitignore

--- KeriAuth.BrowserExtension/wwwroot/scripts/es6/inactivityHelpers.d.ts
+++
@@ -1,7 +0,0 @@
-interface DotNet {
-    invokeMethodAsync<T>(assemblyName: string, methodName: string, ...args: any[]): Promise<T>;
-}
-declare var DotNet: DotNet;
-declare function subscribeToUserInteractions(): void;
-declare function resetInactivityTimer(): void;
-declare function registerLockListener(): void;
