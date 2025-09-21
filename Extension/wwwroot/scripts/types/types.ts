eca324812029084cb6adec801aed42788472a712
Ed Eykholt <8242469+edeykholt@users.noreply.github.com>
2025-09-21 14:03:36 -0700

    removed types.ts

--- Extension/wwwroot/scripts/types/types.ts
+++
@@ -1,6 +0,0 @@
-export interface UpdateDetails {
-    reason: 'update'; // You can add other reasons if needed
-    previousVersion: string;
-    currentVersion: string;
-    timestamp: string; // ISO 8601 formatted string
-}
