b7ac267a514b2e20e8c60ec358c7b74bbdf485b8
Ed Eykholt <8242469+edeykholt@users.noreply.github.com>
2026-02-23 08:37:39 -0800

    removed unused SignifyServiceConfig

--- Extension/Services/SignifyService/SignifyServiceConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Extension.Services.SignifyService {
-    public class SignifyServiceConfig {
-        // See Signify/KERIA Request Authentication Protocol (SKRAP), https://github.com/WebOfTrust/keria/blob/development/docs/protocol.md
-        // public const string MessageRouterPort = "3902"; // aka KERI Protocol Interface, including oobi, and spec.yaml
-        // public const string BootPort = "3903";   // for boot
-        // public const string AdminPort = "3901";  // for agent: API Handler, Agent API Requests, spec.yaml, health
-    }
-}
